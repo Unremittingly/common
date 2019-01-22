180abd300c5eeb64c97a8c46a2b1354a3d642c82
zhangqiang <13608164790zq93>
2019-01-22 10:14:32 +0800

    init

---
+++ formDate/formatDate.js
@@ -0,0 +1,15 @@
+
+
+function formatDate(now) {
+    now = new Date(now);
+    var year=now.getFullYear();
+    var month=now.getMonth()+1;
+    var date=now.getDate();
+    var hour=now.getHours();
+    var minute=now.getMinutes();
+    var second=now.getSeconds();
+    return year+"-"+month+"-"+date+" "+hour+":"+minute+":"+second;
+}
+console.log('formatDate',formatDate(1546592254*1000));
+
+
